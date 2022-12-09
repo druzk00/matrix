cd08c1b91940671f538bf0a8301a6ef6047453fb
druzk00 <119009725+druzk00@users.noreply.github.com>
2022-12-09 23:29:46 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,55 @@
+const canvas = document.getElementById('Matrix');
+const context = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const katakana = '31313131アァカサタاسداسداساساسدقويقويقويقويويقマヤャラワガザダバパイィキシチニヒミリヰギジヂビピウゥクスツヌフムユュルグズブヅプエェケセテネヘメレヱゲゼデベペオォコソトノホモヨョロヲゴゾドボポヴッン';
+const latin = 'سداسداسداسددسآسداسداسدقويقويقويقويويق';
+const nums = '0123456789';
+
+const alphabet = katakana + latin + nums;
+
+const fontSize = 16;
+const columns = canvas.width/fontSize;
+
+const rainDrops = [];
+
+for( let x = 0; x < columns; x++ ) {
+	rainDrops[x] = 1;
+}
+
+const draw = () => {
+	context.fillStyle = 'rgba(0, 0, 0, 0.05)';
+	context.fillRect(0, 0, canvas.width, canvas.height);
+	
+	context.fillStyle = '#0F0';
+	context.font = fontSize + 'px monospace';
+
+	for(let i = 0; i < rainDrops.length; i++)
+	{
+		const text = alphabet.charAt(Math.floor(Math.random() * alphabet.length));
+		context.fillText(text, i*fontSize, rainDrops[i]*fontSize);
+		
+		if(rainDrops[i]*fontSize > canvas.height && Math.random() > 0.975){
+			rainDrops[i] = 0;
+        }
+		rainDrops[i]++;
+	}
+};
+
+setInterval(draw, 30);
+
+function updateErrorText() {
+    // Generate a random string of characters
+    var errorText = "";
+    for (var i = 0; i < 50; i++) {
+      // Use random number to select a character from the list
+      var randomIndex = Math.floor(Math.random() * possibleCharacters.length);
+      errorText += possibleCharacters[randomIndex];
+    }
+  
+    // Update the text in the container element
+    document.getElementById("error-container").innerHTML = errorText;
+  }
+  setInterval(updateErrorText, 100);
